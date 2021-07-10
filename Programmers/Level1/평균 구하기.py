59b2601c78ac53d60f0897c6814298e948d7f338
jiho-kang <43432539+jiho-kang@users.noreply.github.com>
2021-07-10 16:06:09 +0900

    Create 평균 구하기.py

---
+++ Programmers/Level1/평균 구하기.py
@@ -0,0 +1,10 @@
+#https://programmers.co.kr/learn/courses/30/lessons/12944
+
+def solution(arr):
+#     tot = 0
+#     for i in range(len(arr)):
+#         tot += arr[i]
+        
+#     answer = tot/len(arr)
+    answer = sum(arr) / len(arr)
+    return answer
