94ea7ffd2d3cbdbe68c23f6ba02a58cf428a0a89
jiho-kang <mangtojh@naver.com>
2021-07-29 00:37:26 +0900

    Create 두 개 뽑아서 더하기.py

---
+++ Programmers/Level1/두 개 뽑아서 더하기.py
@@ -0,0 +1,12 @@
+# https://programmers.co.kr/learn/courses/30/lessons/68644
+
+# solution
+def solution(numbers):
+    answer = set()
+    for i in range(len(numbers)):
+        for j in range(i+1,len(numbers)):
+            answer.add(numbers[i]+numbers[j])
+    answer = list(answer)
+    answer.sort()
+    
+    return answer
