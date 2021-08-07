ecd2131e472afe213ba802ef1a2ca104d6da35e1
jiho-kang <mangtojh@naver.com>
2021-08-08 00:29:04 +0900

    Create 11047번 동전0.py

---
+++ BaekJoon/11047번 동전0.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/submit/11047
+
+# my solution
+n, k = map(int, input().split())
+
+lst = [int(input()) for _ in range(n)]
+
+lst.sort(reverse=True)
+
+count = 0
+for i in lst:
+  if i <= k:
+    count += k//i
+    k %= i
+    if k == 0:
+      break
+
+print(count)
