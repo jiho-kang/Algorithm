73f5c8768a59ff5ede73ef19afea1a5120ba1f33
jiho-kang <mangtojh@naver.com>
2021-08-08 00:38:47 +0900

    greedy

---
+++ BaekJoon/2875번 대회 or 인턴.py
@@ -0,0 +1,10 @@
+# https://www.acmicpc.net/problem/2875
+
+# solution
+n, m, k = map(int, input().split())
+count = 0
+while n>=2 and m>=1 and n+m >= k+3:
+  n -= 2
+  m -= 1
+  count += 1
+print(count)
