afbdb80fea1cafee2965cbe1d4d79511a8a5eb3b
jiho-kang <mangtojh@naver.com>
2021-10-14 17:56:41 +0900

    Create 기능개발.py

---
+++ Programmers/Level2/기능개발.py
@@ -0,0 +1,22 @@
+# https://programmers.co.kr/learn/courses/30/lessons/42586
+
+# solution
+def solution(progresses, speeds):
+
+    answer = []
+    time = 0
+    count = 0
+    
+    while len(progresses)> 0:
+        if (progresses[0] + time*speeds[0]) >= 100: 
+            progresses.pop(0)
+            speeds.pop(0)
+            count += 1
+            
+        else:
+            if count > 0:
+                answer.append(count)
+                count = 0
+            time += 1
+    answer.append(count)
+    return answer
