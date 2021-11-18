e00b60d1e22b116bad9d144aa00ac033c28b3370
jiho-kang <mangtojh@naver.com>
2021-11-19 00:10:10 +0900

    Update 문자열압축.py

--- Programmers/Level2/문자열압축.py
+++ Programmers/Level2/문자열압축.py
@@ -1,4 +1,4 @@
-https://programmers.co.kr/learn/courses/30/lessons/60057?language=python3
+# https://programmers.co.kr/learn/courses/30/lessons/60057?language=python3
 
 def solution(s):
   if len(s) == 1:
