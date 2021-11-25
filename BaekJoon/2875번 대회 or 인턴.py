a91f42b42a2d2db2b14055f3f57c5b927d3ea678
jiho-kang <mangtojh@naver.com>
2021-11-25 18:58:53 +0900

    greedy

--- BaekJoon/2875번 대회 or 인턴.py
+++ BaekJoon/2875번 대회 or 인턴.py
@@ -8,3 +8,23 @@ while n>=2 and m>=1 and n+m >= k+3:
   m -= 1
   count += 1
 print(count)
+
+
+# my solution
+a, b, k = map(int,input().split())
+
+if a >= 2*b:
+  g = b
+  n = a - (2*b)
+  while n < k:
+    n += 3
+    g -= 1
+  print(g)
+
+else:
+  g = a//2
+  n = (a - (2*g)) + (b - g)
+  while n < k:
+    n += 3
+    g -= 1
+  print(g)
