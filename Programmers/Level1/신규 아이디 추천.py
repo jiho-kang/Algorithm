1ffa7ee7f76e020f47468c5b283642f0fcfedab3
jiho-kang <mangtojh@naver.com>
2022-08-05 15:16:37 +0900

    Update 신규 아이디 추천.py

--- Programmers/Level1/신규 아이디 추천.py
+++ Programmers/Level1/신규 아이디 추천.py
@@ -1,7 +1,22 @@
 # https://programmers.co.kr/learn/courses/30/lessons/72410
 
+# solution1
+import re
 
-# my solution
+def solution(new_id):
+    new_id = new_id.lower()
+    new_id = re.sub('[^a-z0-9\-_.]', '', new_id)
+    while new_id.count('..') > 0:
+        new_id = new_id.replace('..', '.')
+    new_id = re.sub('^[.]|[.]$', '', new_id)
+    new_id = 'a' if len(new_id) == 0 else new_id[:15]
+    new_id = new_id[:15] if len(new_id) > 15 else new_id
+    new_id = new_id[:-1] if new_id[-1] == '.' else new_id
+    while len(new_id) <= 2:
+        new_id += new_id[-1]
+    return new_id
+  
+# solution2
 def solution(new_id):
   new_id = new_id.lower()
   ori = new_id
